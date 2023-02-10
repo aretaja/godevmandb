4977f0c5d43f5f68933070ef30e4c06c68de1b3e
Marko Punnar <marko@aretaja.org>
2023-02-10 17:03:33 +0200

    Add filters to snmp_credentials select * query

--- query/snmp_credentials.sql
+++ query/snmp_credentials.sql
@@ -1,9 +1,28 @@
 -- name: GetSnmpCredentials :many
 SELECT *
 FROM snmp_credentials
-ORDER BY label
-LIMIT $1
-OFFSET $2;
+WHERE (
+    @updated_ge::TIMESTAMPTZ = '0001-01-01 00:00:00+00'
+    OR updated_on >= @updated_ge
+  )
+  AND (
+    @updated_le::TIMESTAMPTZ = '0001-01-01 00:00:00+00'
+    OR updated_on <= @updated_le
+  )
+  AND (
+    @created_ge::TIMESTAMPTZ = '0001-01-01 00:00:00+00'
+    OR created_on >= @created_ge
+  )
+  AND (
+    @created_le::TIMESTAMPTZ = '0001-01-01 00:00:00+00'
+    OR created_on <= @created_le
+  )
+  AND (
+    @label_f::text = ''
+    OR label ILIKE @label_f
+  )
+ORDER BY created_on
+LIMIT NULLIF(@limit_q::int, 0) OFFSET NULLIF(@offset_q::int, 0);
 
 -- name: GetSnmpCredential :one
 SELECT *
